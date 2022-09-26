181da615d15af1f5ffbc61a06d1480afd027d73b
Eric Berry <eric@berry.sh>
2022-09-26 15:17:02 -0600

    Missed changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+if ENV.fetch("COVERAGE", false)
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/spec/"
+    minimum_coverage 90
+    maximum_coverage_drop 2
+  end
+end
+
 require "whoop"
 
 RSpec.configure do |config|
