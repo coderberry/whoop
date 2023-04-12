ac9041408e928626f1eb8b5620e0596bb6687149
delete44 <delete44+tech@protonmail.com>
2023-04-12 18:41:14 +0100

    Added boilerplate for config doc

--- lib/generators/whoop_generator.rb
+++ lib/generators/whoop_generator.rb
@@ -1,5 +1,21 @@
 class WhoopGenerator < Rails::Generators::Base
+  desc "This generator creates an initializer file for the Whoop gem at config/initializers/whoop.rb, with default recommended settings."
+
   def install
-    create_file "config/initializers/whoop.rb", "# Add initialization content here"
+    file_contents = <<~TEXT
+    Whoop.setup do |config|
+      config.logger = ActiveSupport::Logger.new("log/debug.log")
+      # config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+      # config.logger = ActiveSupport::Logger.new($stdout)
+      # config.logger = nil # uses `puts`
+
+      config.level = :debug # or :info, :warn, :error
+      # config.level = :info
+      # config.level = :warn
+      # config.level = :error
+    end
+    TEXT
+
+    create_file "config/initializers/whoop.rb", file_contents
   end
 end
