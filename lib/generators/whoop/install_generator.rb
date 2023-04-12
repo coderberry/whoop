0be4307bdda85a41c555767652b033ebd3a0c3ec
delete44 <delete44+tech@protonmail.com>
2023-04-12 19:18:00 +0100

    Added support for configuring log file

--- lib/generators/whoop/install_generator.rb
+++ lib/generators/whoop/install_generator.rb
@@ -2,11 +2,13 @@ module Whoop
   class InstallGenerator < Rails::Generators::Base
     desc "This generator creates an initializer file for the Whoop gem at config/initializers/whoop.rb, with default recommended settings."
 
+    class_option :logger, type: :string, default: 'log/debug.log'
     class_option :level, type: :string, default: 'debug'
 
     VALID_LEVELS = %w[debug info warn error]
 
     def install
+      @logger = options['logger']
       @level = options['level']
 
       unless VALID_LEVELS.include?(@level)
@@ -16,7 +18,8 @@ module Whoop
 
       file_contents = <<~TEXT
       Whoop.setup do |config|
-        config.logger = ActiveSupport::Logger.new("log/debug.log")
+        config.logger = ActiveSupport::Logger.new("#{@logger}")
+        # config.logger = ActiveSupport::Logger.new("log/debug.log")
         # config.logger = ActiveSupport::Logger.new("log/\#{Rails.env}.log")
         # config.logger = ActiveSupport::Logger.new($stdout)
         # config.logger = nil # uses `puts`
