43dfa8b576f2d7f9943a664d4e7fdcca02270c70
delete44 <delete44+tech@protonmail.com>
2023-04-14 19:46:10 +0100

    Remove config options from generator

--- lib/generators/whoop/install_generator.rb
+++ lib/generators/whoop/install_generator.rb
@@ -1,29 +1,16 @@
 module Whoop
   class InstallGenerator < Rails::Generators::Base
-    desc "This generator creates an initializer file for the Whoop gem at config/initializers/whoop.rb, with default recommended settings."
-
-    VALID_LEVELS = %w[debug info warn error]
-
-    class_option :logger, type: :string, default: "log/debug.log", desc: "Indicates a file to output whoop logs to"
-    class_option :level, type: :string, default: "debug", desc: "Indicates the log level whoop should use. One of #{VALID_LEVELS.join(", ")}"
+    desc "This generator creates an initializer file for the Whoop gem at config/initializers/whoop.rb, with default settings."
 
     def install
-      @logger = options["logger"]
-      @level = options["level"]
-
-      unless VALID_LEVELS.include?(@level)
-        log("Level must be one of #{VALID_LEVELS.join(", ")}\nAborting...\n")
-        exit 1
-      end
-
       file_contents = <<~TEXT
         Whoop.setup do |config|
-          config.logger = ActiveSupport::Logger.new("#{@logger}")
+          config.logger = ActiveSupport::Logger.new("log/debug.log")
           # config.logger = ActiveSupport::Logger.new("log/\#{Rails.env}.log")
           # config.logger = ActiveSupport::Logger.new($stdout)
           # config.logger = nil # uses `puts`
 
-          config.level = :#{@level}
+          config.level = :debug # or :info, :warn, :error
           # config.level = :info
           # config.level = :warn
           # config.level = :error
